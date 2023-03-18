07ac42d6dee617f0ed78bb145adfd3d1c0203de4
Hizkia Jefren Reppi <hizkiajefren0501@gmail.com>
2023-03-19 00:12:32 +0800

    Adding Assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,31 @@
+import IconBreakfast from './icon-breakfast.svg'
+import IconCheckinout from './icon-checkinout.svg'
+import IconElectricity from './icon-electricity.svg'
+import IconExercise from './icon-exercise.svg'
+import IconFacebook from './icon-facebook.svg'
+import IconInstagram from './icon-instagram.svg'
+import IconGoogle from './icon-google.svg'
+import IconLocation from './icon-location.svg'
+import IconOtherService from './icon-otherservice.svg'
+import IconParkingArea from './icon-parkingarea.svg'
+import IconPerson from './icon-person.svg'
+import IconPrivateWorkspace from './icon-privateworkspace.svg'
+import IconSwimmingpool from './icon-swimmingpool.svg'
+import IconWatchVideo from './icon-watchvideo.svg'
+
+export {
+  IconBreakfast,
+  IconCheckinout,
+  IconElectricity,
+  IconExercise,
+  IconFacebook,
+  IconInstagram,
+  IconGoogle,
+  IconLocation,
+  IconOtherService,
+  IconParkingArea,
+  IconPerson,
+  IconPrivateWorkspace,
+  IconSwimmingpool,
+  IconWatchVideo,
+}
