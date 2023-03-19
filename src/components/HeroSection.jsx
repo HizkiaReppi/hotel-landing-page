cd5cb47cff575b8fdacc595921b18ad282167385
anantaw81 <anantaw81@gmail.com>
2023-03-19 12:08:06 +0800

    add dropdown with react select and react datepicker

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,8 +2,41 @@ import { ImageHotel } from '../assets/images';
 import { IconSearch, IconCheckinout, IconLocation, IconPerson, IconWatchVideo } from '../assets/icons';
 import "./herosection.css";
 import Button from './Button';
+import Select from 'react-select';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+
 
 const HeroSection = () => {
+  const [checkInDate, setCheckInDate] = useState(new Date());
+  const [checkOutDate, setCheckOutDate] = useState(new Date());
+
+  const countryOptions= [
+    { value: 'no-selected', label: 'Select...', isFixed: true },
+    { value: 'losangles', label: 'Los Angles' },
+    { value: 'bali', label: 'Bali'},
+    { value: 'sydney', label: 'Sydney' },
+    { value: 'lombok', label: 'Lombok' },
+    { value: 'yogya', label: 'Yogyakarta' },
+    { value: 'singapore', label: 'Singapore' },
+    { value: 'maldiv', label: 'Maldiv' },
+    { value: 'tokyo', label: 'Tokyo' },
+    { value: 'thailand', label: 'Thailand' },
+    { value: 'tokyo', label: 'Tokyo' },
+  ];
+
+  const roomOptions = [
+    { value: 'no-selected', label: 'Select...', isFixed: true },
+    { value: '1room1guest', label: '1 room, 1 guest' },
+    { value: '1room2guests', label: '1 room, 2 guests' },
+    { value: '1room3guests', label: '1 room, 3 guest' },
+    { value: '2room1guest', label: '2 room, 1 guest' },
+    { value: '2room2guests', label: '2 room, 2 guests' },
+    { value: '2room3guests', label: '2 room, 3 guests' },
+  ];
+
+
   return (
     <>
       <div className='hero-section container bg-[#f6fcf9] relative mx-auto mt-8'>
@@ -22,72 +55,98 @@ const HeroSection = () => {
             <img src={ImageHotel} alt="Hotel image"/>
           </div>
         </div>
-        <div className='hero-option absolute right-0 left-0 mx-auto bottom-11 w-4/5 bg-white rounded-[280px] flex justify-between'>
-          <div className='flex justify-between p-6 w-4/5 pl-8'>
-            <div className='hero-location flex justify-start'>
-              <div>
-                <img src={IconLocation} className='mt-1 mr-2' alt="Location Icon" />
-              </div>
-              <div>
-                <div className='text-xl'>
-                  Location
+        <divc className="absolute right-0 left-0 mx-auto bottom-11 w-4/5">
+          <div className='hero-option relative bg-white rounded-[280px] flex justify-between'>
+            <div className='flex justify-between p-6 w-4/5 pl-8 z-1'>
+              <div className='hero-location flex justify-start w-1/5'>
+                <div>
+                  <img src={IconLocation} className='mt-1 mr-2' alt="Location Icon" />
                 </div>
-                <div className='text-sm mt-1 text-[#555555]'>
-                  United States
+                <div>
+                  <div className='text-xl'>
+                    Location
+                  </div>
+                  <div className='text-sm text-[#555555]'>
+                    <Select
+                      id='ccc'
+                      inputId='country-selector'
+                      className="basic-single"
+                      classNamePrefix="select"
+                      defaultValue={countryOptions[0]}
+                      isDisabled={false}
+                      isLoading={false}
+                      isClearable={true}
+                      isSearchable={true}
+                      name="color"
+                      options={countryOptions}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className='vertical-line'></div>
+              <div className='hero-checkin flex justify-start w-1/5'>
+                <div>
+                  <img src={IconCheckinout} className='mt-1 mr-5' alt="CheckIn Icon" />
+                </div>
+                <div>
+                  <div className='text-xl'>
+                    Check In
+                  </div>
+                  <div className='text-sm mt-[6px] text-[#555555]'>
+                    <DatePicker selected={checkInDate} onChange={(date) => setCheckInDate(date)} />
+                  </div>
+                </div>
+              </div>
+              <div className='vertical-line'></div>
+              <div className='hero-checkout flex justify-start w-1/5'>
+                <div>
+                  <img src={IconCheckinout} className='mt-1 mr-5' alt="CheckOut Icon" />
+                </div>
+                <div>
+                  <div className='text-xl '>
+                    Check Out
+                  </div>
+                  <div className='text-sm mt-[6px] text-[#555555]'>
+                    <DatePicker selected={checkOutDate} onChange={(date) => setCheckOutDate(date)} />
+                  </div>
+                </div>
+              </div>
+              <div className='vertical-line'></div>
+              <div className='hero-roomsfor flex justify-start w-1/5'>
+                <div>
+                  <img src={IconPerson} className='mt-1 mr-2' alt="Rooms For Icon" />
+                </div>
+                <div>
+                  <div className='text-xl'>
+                    Rooms for
+                  </div>
+                  <div className='text-sm text-[#555555]'>
+                    <Select
+                      id='ccc'
+                      inputId='country-selector'
+                      className="basic-single"
+                      classNamePrefix="select"
+                      defaultValue={roomOptions[0]}
+                      isDisabled={false}
+                      isLoading={false}
+                      isClearable={true}
+                      isSearchable={true}
+                      name="color"
+                      options={roomOptions}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className='vertical-line'></div>
-            <div className='hero-checkin flex justify-start'>
-              <div>
-                <img src={IconCheckinout} className='mt-1 mr-2' alt="CheckIn Icon" />
-              </div>
-              <div>
-                <div className='text-xl'>
-                  Check In
-                </div>
-                <div className='text-sm mt-1 text-[#555555]'>
-                  10 Jun 2021
-                </div>
-              </div>
-            </div>
-            <div className='vertical-line'></div>
-            <div className='hero-checkout flex justify-start'>
-              <div>
-                <img src={IconCheckinout} className='mt-1 mr-2' alt="CheckOut Icon" />
-              </div>
-              <div>
-                <div className='text-xl '>
-                  Check Out
-                </div>
-                <div className='text-sm mt-1 text-[#555555]'>
-                  15 Jun 2021
-                </div>
-              </div>
-            </div>
-            <div className='vertical-line'></div>
-            <div className='hero-roomsfor flex justify-start'>
-              <div>
-                <img src={IconPerson} className='mt-1 mr-2' alt="Rooms For Icon" />
-              </div>
-              <div>
-                <div className='text-xl'>
-                  Rooms for
-                </div>
-                <div className='text-sm mt-1 text-[#555555]'>
-                  1 room, 2 guests
-                </div>
-              </div>
+            <div className='hero-search flex justify-start relative z-3'>
+              <Button addClass={'!rounded-[280px] flex justify-start items-center text-white text-xl !px-11 w-[300px]'}>
+                <img src={IconSearch} className="mr-2" alt="Search Icon" />
+                Search
+              </Button>
             </div>
           </div>
-          <div className='hero-search flex justify-start'>
-            <Button addClass={'!rounded-[280px] flex justify-start items-center text-white text-xl !px-11'}>
-              <img src={IconSearch} className="mr-2" alt="Search Icon" />
-              Search
-            </Button>
-          </div>
-        </div>
-      </div>
+        </divc>
+      </div>      
     </>
   )
 }
