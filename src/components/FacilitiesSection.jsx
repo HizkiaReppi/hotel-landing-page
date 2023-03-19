7a19d7a511b17532f2e568dca6e828da639eaaea
anantaw81 <anantaw81@gmail.com>
2023-03-19 16:45:05 +0800

    responsive on facilities

--- src/components/FacilitiesSection.jsx
+++ src/components/FacilitiesSection.jsx
@@ -60,15 +60,15 @@ const Facilities = () => {
 
   return (
     <>
-      <div className="container mx-auto flex mt-36">
-        <div className="facilities-left w-2/6 h-full pr-8">
-          <h2 className="leading-[50px] text-[40px] font-semibold">We do our best facilities provide you</h2>
-          <p className="mt-5 text-[16ox] text-[#555555] leading-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy </p>
-          <Button addClass={'mt-12 py-4 px-8 rounded-[36px]'}>
+      <div className="container mx-auto max-md:mx-10 flex mt-36 max-md:flex-col">
+        <div className="facilities-left w-2/6 h-full pr-8 max-md:w-full">
+          <h2 className="leading-[50px] text-[40px] font-semibold max-md:text-2xl">We do our best facilities provide you</h2>
+          <p className="mt-5 text-[16ox] text-[#555555] leading-7 max-md:hidden">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy </p>
+          <Button addClass={'mt-12 py-4 px-8 rounded-[36px] max-md:hidden'}>
             Contact Now
           </Button>
         </div>
-        <div className="facilities-right w-4/6 grid grid-cols-4 gap-4">
+        <div className="facilities-right w-4/6 grid grid-cols-4 gap-4 max-md:w-full max-sm:grid-cols-2 max-sm:gap-2 max-md:grid-cols-3 max-md:mt-7 max-lg:grid-cols-2 max-xl:grid-cols-3">
           {dynamicFacilitiesData.map((data, index) => {
             return <FacilitiesBox key={index} icon={data.icon} title={data.title} addClass=""/>
           })}
